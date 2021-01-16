d9e3757672247a58ccb1ff24d4e735bce56b6813
Brian Nkuru <nkurunungib@gmail.com>
2021-01-15 18:15:19 -0600

    Formarted Ctrl+Alt+L- intelij

--- src/MysqlCon.java
+++ src/MysqlCon.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+
 /*sds
 * jdbc - connecting a java appn to sql db
 //create table emp(id int(10),name varchar(40),age int(3));
@@ -8,27 +9,24 @@ import java.sql.*;
 - connection
 - user name
 - password*/
-class MysqlCon{
-    public static void main(String args[]){
-        try{
-          Class.forName("com.mysql.jdbc.Driver");
+class MysqlCon {
+    public static void main(String args[]) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
 
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/niba","ea","cs544");
-        //here new_schema_jdbc is the database name, root is the username and root is the password
-            Statement stmt=con.createStatement();
-            //if(con != null) System.out.println("Successful connectuin");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/niba", "ea", "cs544");
+            //here new_schema_jdbc is the database name, root is the username and root is the password
+            Statement stmt = con.createStatement();
 
-            ResultSet rs=stmt.executeQuery("select * from emp");
+            ResultSet rs = stmt.executeQuery("select * from emp");
 
-            while(rs.next())
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
 
             con.close();
 
-        }catch(Exception e){ System.out.println(e);
-           /* System.out.println("Download and Install the Download mysql-connector-java jar file\n" +
-                    "\n" +
-                    "paste it in the C:\\Program Files\\Apache Software Foundation\\Tomcat 9.0\\lib");*/}
-
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
